c0e51bc10c3a49e46e4ee316b211662e9aee4d52
Sage Bushstone <sage.bushstone@gmail.com>
2023-11-13 06:50:21 -0500

    Final Project Creation

---
+++ FinalProject_BJSS5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProject_BJSS5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
