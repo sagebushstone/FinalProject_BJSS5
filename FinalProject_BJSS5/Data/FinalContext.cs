89c897eade7422724f4bd01cde0497a15d6660ed
Jasmine <jasminelimsmith30@gmail.com>
2023-11-14 14:36:43 -0500

    readded favorite table

--- FinalProject_BJSS5/Data/FinalContext.cs
+++ FinalProject_BJSS5/Data/FinalContext.cs
@@ -8,6 +8,7 @@ namespace FinalProject_BJSS5.Data
         public FinalContext(DbContextOptions<FinalContext> options) :
             base(options) { }
         public DbSet<Book> Books { get; set;}
+        public DbSet<Favorite> Favorites{ get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -17,6 +18,13 @@ namespace FinalProject_BJSS5.Data
                 new Book { Id = 3, Name = "Sage", Title = "Les Miserables", Author = "Victor Hugo", Publisher = "A. Lacroix, Verboeckhoven & Cie.", PublishYear = 1862 },
                 new Book { Id = 4, Name = "Sydney", Title = "Ballet Shoes", Author = "Noel Streatfeild", Publisher = "J. M. Dent & Sons", PublishYear = 1936 }
                );
+
+            modelBuilder.Entity<Favorite>().HasData(
+              new Favorite { Id = 1, Name = "Bidhatri", Food = "Cheesecake", Color = "Purple", Movie = "Pride and Prejudice", Drink = "Bubble Tea" },
+               new Favorite { Id = 2, Name = "Jasmine", Food = "Sushi", Color = "Green", Movie = "The Meg 2", Drink = "Matcha" },
+               new Favorite { Id = 3, Name = "Sage", Food = "Mashed Potatoes", Color = "Blue", Movie = "First Blood", Drink= "Gold Peak Tea" },
+               new Favorite { Id = 4, Name = "Sydney", Food = "Cucumber", Color = "grey", Movie = "Z-O-M-B-I-E-S", Drink = "Water" }
+              );
         }
     }
 }
--- FinalProject_BJSS5/Models/Favorite.cs
+++ FinalProject_BJSS5/Models/Favorite.cs
@@ -1,6 +1,19 @@
 ﻿namespace FinalProject_BJSS5.Models
 {
     public class Favorite
-    {
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Food { get; set; }
+        public string Color { get; set; }
+        public string Movie { get; set; }
+        public string Drink { get; set; }
+
+        public string GetBooks()
+        {
+            return $"{Id}  {Name}  {Food}  {Color}  {Movie}  {Drink}";
+        }
     }
 }
+    
+
