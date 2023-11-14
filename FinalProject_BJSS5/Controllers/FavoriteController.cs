3b8fd69065e5d8b01ed6ec83e53d4a7601a9cf0a
Sage Bushstone <sage.bushstone@gmail.com>
2023-11-14 16:26:39 -0500

    Added Favorite controller and deleted extraneous Migrations folder

---
+++ FinalProject_BJSS5/Controllers/FavoriteController.cs
@@ -0,0 +1,19 @@
+﻿using FinalProject_BJSS5.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject_BJSS5.Controllers
+{
+    public class FavoriteController : Controller
+    {
+        FinalContext ctx;
+        public FavoriteController(FinalContext ctx)
+        {
+            this.ctx = ctx;
+        }
+        public IActionResult Index()
+        {
+            return Ok(ctx.Favorites.ToList());
+            //return View();
+        }
+    }
+}
